bde74ec8e6197ce683c94f078040b2902266567d
volodymyr-polishchuk <thevivalley@gmail.com>
2018-11-06 22:44:56 +0200

    chore: show user information. close #3

---
+++ api/get_user_info.php
@@ -0,0 +1,18 @@
+<?php
+    include "auth.php";
+    require_once "connect.php";
+
+    $auth_token = $_COOKIE['auth-token'];
+    $auth_k = $_COOKIE['auth-k'];
+
+    $PDO = EIT_DAO::getConnection();
+    $statement = $PDO->prepare("SELECT login, name, description FROM users_eit WHERE k LIKE ? AND password_hash LIKE ? LIMIT 1;");
+    if (!$statement->execute(array($auth_k, $auth_token))) {
+        die($statement->errorInfo());
+    }
+    if ($statement->rowCount() == 0) {
+        die("nothing found");
+    }
+    $json_obj = json_encode($statement->fetchAll(PDO::FETCH_ASSOC)[0]);
+    echo $json_obj;
+
--- js/api_setting.js
+++ js/api_setting.js
@@ -1,5 +1,33 @@
+//init
+
+window.onload = function() {
+    let userLogin = document.getElementById("user_login");
+    let userName = document.getElementById("user_name");
+    let description = document.getElementById("description");
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'api/get_user_info.php', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) return;
+        if (xhr.status === 200) {
+            let response = JSON.parse(xhr.responseText);
+            userLogin.innerText = response.login;
+            userName.innerText = response.name;
+            description.innerText = response.description;
+        } else {
+            handleException(xhr.status);
+        }
+    };
+    xhr.send();
+};
+
 function onLogoutClick() {
     setCookie('auth-token', null, {expires: -1});
     setCookie('auth-k', null, {expires: -1});
     document.location.href = 'login.html';
+}
+
+function handleException(code) {
+    if (code === 403) {
+        document.location = "https://hwork.net/polishchuk/eit/login.html";
+    }
 }
